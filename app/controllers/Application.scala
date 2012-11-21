336096d0a32d0fcb379a5a35bb28bb8fb4e3d59a
Jean Helou <jean.helou@gmail.com>
2012-11-21 20:26:39 +0100

    gogogogo

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,8 +3,55 @@ package controllers
 import play.api._
 import play.api.mvc._
 
-object Application extends Controller {
-  def index = Action {
-    Ok("index")
+/**
+ * Provide security features
+ */
+trait Secured {
+  /**
+   * Retrieve the connected user email.
+   */
+  private def username(request: RequestHeader) = request.session.get("email")
+
+  /**
+   * Redirect to login if the user in not authorized.
+   */
+  private def onUnauthorized(request: RequestHeader) = {
+    //Logger.info("Unauthorized access to " + request.uri + " , redirecting to " + routes.Application.login())
+   // Results.Redirect(routes.Application.login()).withSession("before_auth_requested_url" -> request.uri)
+    Results.Redirect("")
+  }
+
+  // ---
+
+  /**
+   * Action for authenticated users.
+   */
+  def IsAuthenticated(f: => String => Request[AnyContent] => Result): Action[(Action[AnyContent], AnyContent)] = Security.Authenticated(username, onUnauthorized) {
+    userId =>
+      Action({
+        request =>
+          Logger.info("Authorized access to " + request.uri + " , for user " + userId)
+          f(userId)(request)
+      })
+  }
+
+  /**
+   * Action for authenticated users.
+   */
+  def IsAuthenticated[A](parser: BodyParser[A])(f: => String => Request[A] => Result) = Security.Authenticated(username, onUnauthorized) {
+    user =>
+      Action(parser)(request => f(user)(request))
   }
 }
+
+trait XStaffing {
+  this: Controller with Secured=>
+  def index = IsAuthenticated { userid => implicit request =>
+    controllers.Assets.at("/public", "index").apply(request)
+  }
+}
+
+object Application extends Controller with Secured with XStaffing
+
+
+
--- project/Build.scala
+++ project/Build.scala
@@ -4,12 +4,27 @@ import Keys._
 
 object ApplicationBuild extends Build {
 
-  val appName = "restapp"
+  val appName = "xstaffing"
   val appVersion = "1.0"
 
-  val appDependencies = Seq()
+  val exclusions = Seq(
+    ExclusionRule("org.hibernate.javax.persistence","hibernate-jpa-2.0-api"),
+    ExclusionRule(name="anorm_2.9.1"),
+    ExclusionRule(name="ebean"),
+    ExclusionRule(name="ehcache-core"),
+    ExclusionRule(name="h2"),
+    ExclusionRule(name="bonecp"),
+    ExclusionRule(name="guava")
+  )
+
+  var appDependencies: Seq[ModuleID] =Seq()
 
   val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-  )
+     libraryDependencies ~=  { _.map {
+	  case dep if dep.name=="play"  => exclusions.foldLeft(dep){ (depWithExclusions, ex) => depWithExclusions.excludeAll(ex)}
+	  case dep => dep
+	}}
+  ) 
+ 
 
 }
